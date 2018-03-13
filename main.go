955475d8a89b5b23422483965b72b294677ba694
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2018-03-13 19:57:38 -0400

    add multi file support

--- main.go
+++ main.go
@@ -24,47 +24,53 @@ func main() {
 }
 
 func runCmdFunc(cmd *cobra.Command, args []string) error {
-	prog, err := jq.New()
-	if err != nil {
-		return fmt.Errorf("failed to initialize libjq: %s", err)
-	}
-	defer prog.Close()
+	program := args[0]
+	pathArgs := args[1:]
 
-	path := os.ExpandEnv(args[1])
-	fileBytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		return fmt.Errorf("failed to read file at %s: `%s`", path, err)
-	}
-
-	formatName, err := cmd.Flags().GetString("format")
-	if err != nil {
-		panic("failed to find format flag")
-	}
-
-	unmarshaledFile, err := unmarshal(formatName, fileBytes)
-	if err != nil {
-		return fmt.Errorf("failed to unmarshal file at %s: `%s`", path, err)
-	}
-
-	fileJv, err := jq.JvFromInterface(unmarshaledFile)
-	if err != nil {
-		panic("failed to reflect a jv from unmarshalled file")
-	}
-
-	errs := prog.Compile(args[0], jq.JvArray())
-	for _, err := range errs {
+	for _, pathArg := range pathArgs {
+		libjq, err := jq.New()
 		if err != nil {
-			return fmt.Errorf("failed to compile jq program for file at %s: %s", path, err)
+			return fmt.Errorf("failed to initialize libjq: %s", err)
 		}
-	}
+		// Sucks these won't close until runCmdFunc exits.
+		defer libjq.Close()
 
-	resultJvs, err := prog.Execute(fileJv)
-	if err != nil {
-		return fmt.Errorf("failed to execute jq program for file at %s: %s", path, err)
-	}
+		path := os.ExpandEnv(pathArg)
+		fileBytes, err := ioutil.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("failed to read file at %s: `%s`", path, err)
+		}
 
-	for _, resultJv := range resultJvs {
-		fmt.Println(resultJv.Dump(jq.JvPrintPretty | jq.JvPrintSpace1 | jq.JvPrintColour))
+		formatName, err := cmd.Flags().GetString("format")
+		if err != nil {
+			panic("failed to find format flag")
+		}
+
+		unmarshaledFile, err := unmarshal(formatName, fileBytes)
+		if err != nil {
+			return fmt.Errorf("failed to unmarshal file at %s: `%s`", path, err)
+		}
+
+		fileJv, err := jq.JvFromInterface(unmarshaledFile)
+		if err != nil {
+			panic("failed to reflect a jv from unmarshalled file")
+		}
+
+		errs := libjq.Compile(program, jq.JvArray())
+		for _, err := range errs {
+			if err != nil {
+				return fmt.Errorf("failed to compile jq program for file at %s: %s", path, err)
+			}
+		}
+
+		resultJvs, err := libjq.Execute(fileJv)
+		if err != nil {
+			return fmt.Errorf("failed to execute jq program for file at %s: %s", path, err)
+		}
+
+		for _, resultJv := range resultJvs {
+			fmt.Println(resultJv.Dump(jq.JvPrintPretty | jq.JvPrintSpace1 | jq.JvPrintColour))
+		}
 	}
 
 	return nil
