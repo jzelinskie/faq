77b1ac4a308410ddc40fce175a8f55097b8caed1
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2018-05-22 18:27:20 -0400

    jq: refactor tests
    This removes the testing library and makes certain tests table driven.

--- jq/jv_test.go
+++ jq/jv_test.go
@@ -22,205 +22,308 @@
 package jq_test
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/cheekybits/is"
 
 	"github.com/jzelinskie/faq/jq"
 )
 
 func TestJvKind(t *testing.T) {
-	is := is.New(t)
-
-	cases := []struct {
+	table := []struct {
+		testName string
 		*jq.Jv
 		jq.JvKind
-		string
+		stringKind string
 	}{
-		{jq.JvNull(), jq.JvKindNull, "null"},
-		{jq.JvFromString("a"), jq.JvKindString, "string"},
+		{"Null", jq.JvNull(), jq.JvKindNull, "null"},
+		{"FromString", jq.JvFromString("a"), jq.JvKindString, "string"},
 	}
 
-	for _, c := range cases {
-		defer c.Free()
-		is.Equal(c.Kind(), c.JvKind)
-		is.Equal(c.Kind().String(), c.string)
+	for _, tt := range table {
+		t.Run(tt.testName, func(t *testing.T) {
+			defer tt.Free()
+			if tt.Kind() != tt.JvKind {
+				t.Errorf("JvKind() got: %v, want: %v", tt.Kind(), tt.JvKind)
+			}
+
+			if tt.Kind().String() != tt.stringKind {
+				t.Errorf("JvKind().String() got: %s, want: %s", tt.Kind().String(), tt.stringKind)
+			}
+		})
 	}
 }
 
 func TestJvString(t *testing.T) {
-	is := is.New(t)
-
 	jv := jq.JvFromString("test")
 	defer jv.Free()
 
 	str, err := jv.String()
+	if err != nil {
+		t.Errorf("error when converting jv into string: %s", err)
+	}
+	if str != "test" {
+		t.Errorf(`jvFromString("test") got: %s, want: test`, str)
+	}
 
-	is.Equal(str, "test")
-	is.NoErr(err)
-
-	i := jv.ToGoVal()
-
-	is.Equal(i, "test")
+	if jv.ToGoVal() != "test" {
+		t.Errorf(`jvFromString("test").ToGoVal() got: %s, want: test`, jv.ToGoVal())
+	}
 }
 
 func TestJvStringOnNonStringType(t *testing.T) {
-	is := is.New(t)
-
-	// Test that on a non-string value we get a go error, not a C assert
 	jv := jq.JvNull()
 	defer jv.Free()
 
-	_, err := jv.String()
-	is.Err(err)
+	if _, err := jv.String(); err == nil {
+		t.Errorf("created string from jv null value")
+	}
 }
 
 func TestJvFromJSONString(t *testing.T) {
-	is := is.New(t)
-
 	jv, err := jq.JvFromJSONString("[]")
-	is.NoErr(err)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindArray)
+	if err != nil {
+		t.Errorf("error when parsing jv from JSON string: %s", err)
+	}
+	if jv == nil {
+		t.Errorf(`nil jv when parsing from JSON string "[]"`)
+	}
+
+	if jv.Kind() != jq.JvKindArray {
+		t.Errorf(`jv kind is not Array for JSON string "[]"`)
+	}
 
 	jv, err = jq.JvFromJSONString("not valid")
-	is.Err(err)
-	is.Nil(jv)
+	if err == nil {
+		t.Errorf("parsing jv succeeded when parsing invalid JSON")
+	}
+	if jv != nil {
+		t.Errorf("jv value was not nil when parsing invalid JSON")
+	}
 }
 
 func TestJvFromFloat(t *testing.T) {
-	is := is.New(t)
+	const exampleFloat = 1.23
 
-	jv := jq.JvFromFloat(1.23)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindNumber)
+	jv := jq.JvFromFloat(exampleFloat)
+	if jv.Kind() != jq.JvKindNumber {
+		t.Errorf("jv kind is not Number when initialized by a float")
+	}
+
 	gv := jv.ToGoVal()
 	n, ok := gv.(float64)
-	is.True(ok)
-	is.Equal(n, float64(1.23))
+	if !ok {
+		t.Errorf("failed to cast jv float to Go float64")
+	}
+	if n != float64(exampleFloat) {
+		t.Errorf("float value casted from jv is not equal to original Go value")
+	}
 }
 
 func TestJvFromInterface(t *testing.T) {
-	is := is.New(t)
-
 	// Null
 	jv, err := jq.JvFromInterface(nil)
-	is.NoErr(err)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindNull)
+	if err != nil {
+		t.Errorf("JvFromInterface() with nil failed to parse: %s", err)
+	}
+	if jv == nil {
+		t.Errorf("JvFromInterface() with nil suceeded, but returned nil")
+	}
+	if jv.Kind() != jq.JvKindNull {
+		t.Errorf("JvFromInterface() with nil did not parse into a JvKindNull")
+	}
 
-	// Boolean
+	// Boolean true
 	jv, err = jq.JvFromInterface(true)
-	is.NoErr(err)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindTrue)
+	if err != nil {
+		t.Errorf("JvFromInterface() with true failed to parse: %s", err)
+	}
+	if jv == nil {
+		t.Errorf("JvFromInterface() with true suceeded, but returned nil")
+	}
+	if jv.Kind() != jq.JvKindTrue {
+		t.Errorf("JvFromInterface() with true did not parse into a JvKindTrue")
+	}
 
+	// Boolean false
 	jv, err = jq.JvFromInterface(false)
-	is.NoErr(err)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindFalse)
+	if err != nil {
+		t.Errorf("JvFromInterface() with false failed to parse: %s", err)
+	}
+	if jv == nil {
+		t.Errorf("JvFromInterface() with false suceeded, but returned nil")
+	}
+	if jv.Kind() != jq.JvKindFalse {
+		t.Errorf("JvFromInterface() with false did not parse into a JvKindFalse")
+	}
 
 	// Float
 	jv, err = jq.JvFromInterface(1.23)
-	is.NoErr(err)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindNumber)
+	if err != nil {
+		t.Errorf("JvFromInterface() with a float failed to parse: %s", err)
+	}
+	if jv == nil {
+		t.Errorf("JvFromInterface() with a float suceeded, but returned nil")
+	}
+	if jv.Kind() != jq.JvKindNumber {
+		t.Errorf("JvFromInterface() with a float did not parse into a JvKindNumber")
+	}
 	gv := jv.ToGoVal()
 	n, ok := gv.(float64)
-	is.True(ok)
-	is.Equal(n, float64(1.23))
+	if !ok {
+		t.Errorf("JVFromInterface() with a float failed to cast back to Go value")
+	}
+	if n != float64(1.23) {
+		t.Errorf("JVFromInterface() with a float casted back is not equal to original Go value")
+	}
 
 	// Integer
 	jv, err = jq.JvFromInterface(456)
-	is.NoErr(err)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindNumber)
+	if err != nil {
+		t.Errorf("JvFromInterface() with an integer failed to parse: %s", err)
+	}
+	if jv == nil {
+		t.Errorf("JvFromInterface() with an integer suceeded, but returned nil")
+	}
+	if jv.Kind() != jq.JvKindNumber {
+		t.Errorf("JvFromInterface() with an integer did not parse into a JvKindNumber")
+	}
 	gv = jv.ToGoVal()
 	n2, ok := gv.(int)
-	is.True(ok)
-	is.Equal(n2, 456)
+	if !ok {
+		t.Errorf("JVFromInterface() with an integer failed to cast back to Go value")
+	}
+	if n2 != 456 {
+		t.Errorf("JVFromInterface() with an integer casted back is not equal to original Go value")
+	}
 
 	// String
 	jv, err = jq.JvFromInterface("test")
-	is.NoErr(err)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindString)
+	if err != nil {
+		t.Errorf("JvFromInterface() with a string failed to parse: %s", err)
+	}
+	if jv == nil {
+		t.Errorf("JvFromInterface() with a string suceeded, but returned nil")
+	}
+	if jv.Kind() != jq.JvKindString {
+		t.Errorf("JvFromInterface() with a string did not parse into a JvKindString")
+	}
 	gv = jv.ToGoVal()
 	s, ok := gv.(string)
-	is.True(ok)
-	is.Equal(s, "test")
+	if !ok {
+		t.Errorf("JVFromInterface() with a string failed to cast back to Go value")
+	}
+	if s != "test" {
+		t.Errorf("JVFromInterface() with a string casted back is not equal to original Go value")
+	}
 
 	jv, err = jq.JvFromInterface([]string{"test", "one", "two"})
-	is.NoErr(err)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindArray)
+	if err != nil {
+		t.Errorf("JvFromInterface() with an array failed to parse: %s", err)
+	}
+	if jv == nil {
+		t.Errorf("JvFromInterface() with an array suceeded, but returned nil")
+	}
+	if jv.Kind() != jq.JvKindArray {
+		t.Errorf("JvFromInterface() with an array did not parse into a JvKindArray")
+	}
 	gv = jv.ToGoVal()
-	is.Equal(gv.([]interface{})[2], "two")
+	if gv.([]interface{})[2] != "two" {
+		t.Errorf("JVFromInterface() with an array casted back is not equal to original Go value")
+	}
 
 	jv, err = jq.JvFromInterface(map[string]int{"one": 1, "two": 2})
-	is.NoErr(err)
-	is.OK(jv)
-	is.Equal(jv.Kind(), jq.JvKindObject)
+	if err != nil {
+		t.Errorf("JvFromInterface() with a map failed to parse: %s", err)
+	}
+	if jv == nil {
+		t.Errorf("JvFromInterface() with a map suceeded, but returned nil")
+	}
+	if jv.Kind() != jq.JvKindObject {
+		t.Errorf("JvFromInterface() with a map did not parse into a JvKindObject")
+	}
 	gv = jv.ToGoVal()
-	is.Equal(gv.(map[string]interface{})["two"], 2)
+	if gv.(map[string]interface{})["two"] != 2 {
+		t.Errorf("JVFromInterface() with a map casted back is not equal to original Go value")
+	}
 }
 
 func TestJvDump(t *testing.T) {
-	is := is.New(t)
+	table := []struct {
+		input  string
+		flags  jq.JvPrintFlags
+		output string
+	}{
+		{"test", jq.JvPrintNone, `"test"`},
+		{"test", jq.JvPrintColour, "\x1b[0;32m" + `"test"` + "\x1b[0m"},
+	}
+	for _, tt := range table {
+		t.Run(fmt.Sprintf("%s-%b", tt.input, tt.flags), func(t *testing.T) {
+			jv := jq.JvFromString(tt.input)
+			defer jv.Free()
 
-	jv := jq.JvFromString("test")
-	defer jv.Free()
-
-	dump := jv.Copy().Dump(jq.JvPrintNone)
-
-	is.Equal(`"test"`, dump)
-	dump = jv.Copy().Dump(jq.JvPrintColour)
-
-	is.Equal([]byte("\x1b[0;32m"+`"test"`+"\x1b[0m"), []byte(dump))
+			dump := jv.Copy().Dump(tt.flags)
+			if dump != tt.output {
+				t.Errorf("dump not equal to expected got: %#v want: %#v", dump, tt.output)
+			}
+		})
+	}
 }
 
 func TestJvInvalid(t *testing.T) {
-	is := is.New(t)
-
 	jv := jq.JvInvalid()
+	if jv.IsValid() == true {
+		t.Errorf("IsValid() returned true for JvInvalid()")
+	}
 
-	is.False(jv.IsValid())
+	if _, ok := jv.Copy().GetInvalidMessageAsString(); ok {
+		t.Errorf("GetInvalidMessageAsString() returned string for no value")
 
-	_, ok := jv.Copy().GetInvalidMessageAsString()
-	is.False(ok) // "Expected no Invalid message"
+	}
 
-	jv = jv.GetInvalidMessage()
-	is.Equal(jv.Kind(), jq.JvKindNull)
+	if jv.GetInvalidMessage().Kind() != jq.JvKindNull {
+		t.Errorf("GetInvalidMessage().Kind() returned a kind other than JvKindNull")
+	}
 }
 
 func TestJvInvalidWithMessage_string(t *testing.T) {
-	is := is.New(t)
-
-	jv := jq.JvInvalidWithMessage(jq.JvFromString("Error message 1"))
-
-	is.False(jv.IsValid())
+	errMsg := "Error message 1"
+	jv := jq.JvInvalidWithMessage(jq.JvFromString(errMsg))
+	if jv.IsValid() == true {
+		t.Errorf("IsValid() returned true for JvInvalidWithMessage()")
+	}
 
 	msg := jv.Copy().GetInvalidMessage()
-	is.Equal(msg.Kind(), jq.JvKindString)
+	if msg.Kind() != jq.JvKindString {
+		t.Errorf("JvInvalidWithMessage().GetInvalidMessage().Kind() returned a kind other than JvKindString")
+	}
 	msg.Free()
 
 	str, ok := jv.GetInvalidMessageAsString()
-	is.True(ok)
-	is.Equal("Error message 1", str)
+	if !ok {
+		t.Errorf("JvInvalidWithMessage().JvGetInvalidMessageAsString() is not ok")
+	}
+	if str != errMsg {
+		t.Errorf("JvInvalidWithMessage().JvGetInvalidMessageAsString() did not return original error message")
+	}
 }
 
 func TestJvInvalidWithMessage_object(t *testing.T) {
-	is := is.New(t)
-
 	jv := jq.JvInvalidWithMessage(jq.JvObject())
-
-	is.False(jv.IsValid())
+	if jv.IsValid() == true {
+		t.Errorf("IsValid() returned true for JvInvalidWithMessage()")
+	}
 
 	msg := jv.Copy().GetInvalidMessage()
-	is.Equal(msg.Kind(), jq.JvKindObject)
+	if msg.Kind() != jq.JvKindObject {
+		t.Errorf("JvInvalidWithMessage().GetInvalidMessage().Kind() returned a kind other than JvKindObject")
+	}
 	msg.Free()
 
 	str, ok := jv.GetInvalidMessageAsString()
-	is.True(ok)
-	is.Equal("{}", str)
+	if !ok {
+		t.Errorf("JvInvalidWithMessage().JvGetInvalidMessageAsString() is not ok")
+	}
+	if str != "{}" {
+		t.Errorf(`JvInvalidWithMessage().JvGetInvalidMessageAsString() did not return "{}"`)
+	}
 }
