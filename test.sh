2d0af45fffaf80a3c4db979d9e27d7c9613b202d
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2019-01-22 21:30:01 -0500

    upgrade to go modules

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 
-dep ensure
+go mod vendor
 go vet $(go list ./...)
 diff <(goimports -d $(find . -type f -name '*.go' -not -path "./vendor/*")) <(printf "")
 (for d in $(go list ./...); do diff <(golint $d) <(printf "") || exit 1;  done)
