2f2a5023d1fa515802fa215e1e7d6a9449d71308
Chance Zibolski <czibolsk@redhat.com>
2019-01-24 15:05:09 -0800

    Support yaml document streams and JSON value streams
    Support multiple yaml documents in a single file/stream separated by `---`
    
    Use go-yaml Decoder to handle document streams and copy ghodss/yaml
    convertToJSONableObject to formats/yaml_helpers.go to enable converting
    yaml to json.
    
    Also support JSON streams
    
    go.mod: Add gopkg.in/yaml.v2 explicitly

--- main_test.go
+++ main_test.go
@@ -31,6 +31,40 @@ func TestRunFaq(t *testing.T) {
 			},
 		},
 		{
+			name:    "single file multiple simple object json stream simple program",
+			program: ".",
+			inputFileContents: []string{
+				"{}\n{}\n",
+			},
+			expectedOutput: "{}\n{}\n",
+			flags: flags{
+				inputFormat:  "json",
+				outputFormat: "json",
+			},
+		},
+		{
+			name:    "single file multiple complex object json stream simple program",
+			program: ".",
+			inputFileContents: []string{
+				`{}
+{
+}
+{
+   "bar": 2,
+   "foo": true
+}
+`,
+			},
+			expectedOutput: `{}
+{}
+{"bar":2,"foo":true}
+`,
+			flags: flags{
+				inputFormat:  "json",
+				outputFormat: "json",
+			},
+		},
+		{
 			name:    "single file empty string simple program",
 			program: ".",
 			inputFileContents: []string{
@@ -39,6 +73,81 @@ func TestRunFaq(t *testing.T) {
 			expectedOutput: `""` + "\n",
 			flags: flags{
 				inputFormat:  "json",
+				outputFormat: "json",
+			},
+		},
+		{
+			name:    "single file yaml stream simple program",
+			program: ".",
+			inputFileContents: []string{
+				`---
+foo: true
+---
+bar: false`,
+			},
+			expectedOutput: `{"foo":true}
+{"bar":false}
+`,
+			flags: flags{
+				inputFormat:  "yaml",
+				outputFormat: "json",
+			},
+		},
+		{
+			// TODO: this should return "" as the output probably
+			name:    "FIXME single file yaml single empty yaml stream simple program",
+			program: ".",
+			inputFileContents: []string{
+				`---
+`,
+			},
+			expectedOutput: `null
+`,
+			flags: flags{
+				inputFormat:  "yaml",
+				outputFormat: "json",
+			},
+		},
+		{
+			// TODO: this should return "" as the output probably
+			name:    "FIXME single file yaml multiple empty yaml stream simple program",
+			program: ".",
+			inputFileContents: []string{
+				`---
+---
+---
+`,
+			},
+			expectedOutput: "null\nnull\nnull\n",
+			flags: flags{
+				inputFormat:  "yaml",
+				outputFormat: "json",
+			},
+		},
+		{
+			name:    "single file yaml stream with extra newlines simple program",
+			program: ".",
+			inputFileContents: []string{
+				// these extra newlines are intentionally here to ensure
+				// they're not treated specially
+				`
+
+
+---
+
+foo: true
+
+---
+
+bar: false
+
+`,
+			},
+			expectedOutput: `{"foo":true}
+{"bar":false}
+`,
+			flags: flags{
+				inputFormat:  "yaml",
 				outputFormat: "json",
 			},
 		},
@@ -110,6 +219,51 @@ func TestRunFaq(t *testing.T) {
 				slurp:        true,
 			},
 		},
+		{
+			name:    "slurp multiple file json stream",
+			program: ".",
+			inputFileContents: []string{
+				`
+{}
+{}
+{
+	"bar": 2,
+	"foo": true
+}
+`,
+				``,   // empty files are ignored
+				`""`, // an empty string is valid
+				`true`,
+			},
+			expectedOutput: `[{},{},{"bar":2,"foo":true},"",true]` + "\n",
+			flags: flags{
+				inputFormat:  "json",
+				outputFormat: "json",
+				slurp:        true,
+			},
+		},
+		{
+			name:    "slurp multiple file yaml stream simple program",
+			program: ".",
+			inputFileContents: []string{
+				`---
+foo: true
+---
+bar: false`,
+				`---
+fizz: buzz
+---
+cats: dogs
+`,
+			},
+			expectedOutput: `[{"foo":true},{"bar":false},{"fizz":"buzz"},{"cats":"dogs"}]
+`,
+			flags: flags{
+				inputFormat:  "yaml",
+				outputFormat: "json",
+				slurp:        true,
+			},
+		},
 	}
 
 	for _, testCase := range testCases {
@@ -128,6 +282,7 @@ func TestRunFaq(t *testing.T) {
 			if err != nil {
 				t.Errorf("expected no err, got %#v", err)
 			}
+
 			output := outputBuf.String()
 			if output != testCase.expectedOutput {
 				t.Errorf("incorrect output expected=%s, got=%s", testCase.expectedOutput, output)
